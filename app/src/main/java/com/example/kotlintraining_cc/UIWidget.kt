2ee86760ef187a8b707172621c0d8613cfabaa3f
sapna73 <sg.sapna73@gmail.com>
2021-09-16 12:51:11 +0530

    Buttons, Views

---
+++ app/src/main/java/com/example/kotlintraining_cc/UIWidget.kt
@@ -0,0 +1,11 @@
+package com.example.kotlintraining_cc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UIWidget : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uiwidget)
+    }
+}
